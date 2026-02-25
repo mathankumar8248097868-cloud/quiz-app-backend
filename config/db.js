3f0925a2af70639f169902807c4fcab8d09e7316
mathankumar8248097868-cloud <mathankumar8248097868@gmail.com>
2026-02-25 21:20:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -6,6 +7,9 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  },
   connectTimeout: 60000
 });
 
